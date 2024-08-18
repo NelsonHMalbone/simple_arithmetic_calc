e9aacf359d00d2b86bfef30a6f5ec4eb45570696
Nelson Malbone <nhmalbone@icloud.com>
2024-08-18 17:16:11 -0400

    Date: 8-28-2024 Simple_Arithmetic_Calc
    worked on a problem for 0/0 so now it will print a error message alone with if the user enters a non operater it will also quit

--- main2.py
+++ main2.py
@@ -1,28 +1,37 @@
 # python Calc using a match case instead of if
 
 
-operator = input("Enter a Operator + - * /: ")
+operator = input("Enter a Operator + - * / or 'Quit' to exit: ")
 numb_1 = float(input("Enter the first number: "))
 numb_2 = float(input("Enter the second number: "))
-numb_3 = float(input("Enter the third number: "))
 
 
 
-match operator:
-    case "+":
-        result = numb_1 + numb_2 + numb_3
-        print(round(result, 3))
-    case "-":
-        result = numb_1 - numb_2 - numb_3
-        print(round(result, 3))
-    case "*":
-        result = numb_1 * numb_2 * numb_3
-        print(round(result, 3))
-    case "/":
-        result = numb_1 / numb_2 / numb_3
-        print(round(result, 3))
-        # this is the else for match case
-    case _:
-        print(f'{operator} is not a vaild operator')
-
+while True:
+    try:
+        match operator:
+            case "Quit":
+                print("Exiting the program")
+                break
+            case "+":
+                result = numb_1 + numb_2
+                print(round(result, 3))
+            case "-":
+                result = numb_1 - numb_2
+                print(round(result, 3))
+            case "*":
+                result = numb_1 * numb_2
+                print(round(result, 3))
+            case "/":
+                try:
+                    result = numb_1 / numb_2
+                    print(round(result, 3))
+                    # this is the else for match case
+                except ZeroDivisionError:
+                    print("Error: Division by zero is not")
+                    break
+            case _:
+                print(f'{operator} is not a vaild operator')
+    except TypeError:
+        print("Error: Invalid input types.")
 
