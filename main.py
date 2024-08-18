e3a22acf58a6bf6b49bb36cdaf7f0f6e74253cdf
Nelson Malbone <nhmalbone@icloud.com>
2024-08-18 15:18:56 -0400

    Date: 8-28-2024 Simple_Arithmetic_Calc used string concatenation

---
+++ main.py
@@ -0,0 +1,19 @@
+# Python Calculator.
+#BroCode https://youtu.be/4wGuB3oAKc4?si=ZGdoU7UFO80AGcKU
+
+
+# user to select a arthimetic operator
+operator = input("Enter a operator (+ - * /): ")
+
+# create a var for the first and second number
+# this forms string concatenation
+# numb1 = input("Enter First number: ")
+# numb2 = input("Enter Second number: ")
+
+# print(numb1 + numb2) # string concatenation just join two strings together
+
+# comes up with 1011
+# accepting user input there accepted as sting data imports
+
+# test print with just
+# num1 + num2
