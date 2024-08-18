1bdb81eabee538babc82d9f1eb1c78794cf82460
Nelson Malbone <nhmalbone@icloud.com>
2024-08-18 16:13:17 -0400

    Date: 8-28-2024 Simple_Arithmetic_Calc
    added all operators to the case statements
    with a error statement

--- main2.py
+++ main2.py
@@ -1,26 +1,26 @@
 # python Calc using a match case instead of if
 
 
-
+operator = input("Enter a Operator + - * /: ")
 numb_1 = float(input("Enter the first number: "))
 numb_2 = float(input("Enter the second number: "))
 numb_3 = float(input("Enter the third number: "))
 
 
-operator = input("Enter a Operator + - * /")
+
 match operator:
     case "+":
         result = numb_1 + numb_2 + numb_3
-        print(round(result), 3)
+        print(round(result, 3))
     case "-":
         result = numb_1 - numb_2 - numb_3
-        print(round(result), 3)
+        print(round(result, 3))
     case "*":
         result = numb_1 * numb_2 * numb_3
-        print(round(result), 3)
+        print(round(result, 3))
     case "/":
         result = numb_1 / numb_2 / numb_3
-        print(round(result), 3)
+        print(round(result, 3))
         # this is the else for match case
     case _:
         print(f'{operator} is not a vaild operator')
