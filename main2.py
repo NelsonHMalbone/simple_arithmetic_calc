8cf3b04960d933d9db9b1c581eb6434512fb43fa
Nelson Malbone <nhmalbone@icloud.com>
2024-08-18 17:24:02 -0400

    Date: 8-28-2024 Simple_Arithmetic_Calc
    added a break so it would stop

--- main2.py
+++ main2.py
@@ -10,26 +10,30 @@ numb_2 = float(input("Enter the second number: "))
 while True:
     try:
         match operator:
-            case "Quit":
-                print("Exiting the program")
-                break
             case "+":
                 result = numb_1 + numb_2
                 print(round(result, 3))
+                break
             case "-":
                 result = numb_1 - numb_2
                 print(round(result, 3))
+                break
             case "*":
                 result = numb_1 * numb_2
                 print(round(result, 3))
+                break
             case "/":
                 try:
                     result = numb_1 / numb_2
                     print(round(result, 3))
+                    break
                     # this is the else for match case
                 except ZeroDivisionError:
                     print("Error: Division by zero is not")
                     break
+            case "Quit":
+                print("Exiting the program")
+                break
             case _:
                 print(f'{operator} is not a vaild operator')
     except TypeError:
