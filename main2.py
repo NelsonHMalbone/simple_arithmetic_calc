e5bcfe859684dbef265dabdf888d3aea71b9b5a4
Nelson Malbone <nhmalbone@icloud.com>
2024-08-18 15:59:29 -0400

    Date: 8-28-2024 Simple_Arithmetic_Calc using a match case statements

---
+++ main2.py
@@ -0,0 +1,8 @@
+# python Calc using a match case instead of if
+
+operator = input("Enter a Operator + - * /")
+
+numb_1 = float(input("Enter the first number: "))
+numb_2 = float(input("Enter the second number: "))
+numb_3 = float(input("Enter the third number: "))
+
